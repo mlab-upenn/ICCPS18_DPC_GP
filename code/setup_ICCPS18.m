5f64001219144e642af43b074717d7787f0c7876
Truong Nghiem <truong.nghiem@gmail.com>
2017-08-29 18:41:13 -0400

    Add setup_ICCPS18.m scrip to setup code base for Matlab.

---
+++ code/setup_ICCPS18.m
@@ -0,0 +1,33 @@
+% Setup the toolboxes used by the ICCPS'18 code, for Matlab.
+
+me = mfilename;                                            % what is my filename
+disp(mfilename);
+disp(me);
+mydir = which(me); mydir = mydir(1:end-3-numel(me));        % where am I located
+
+% GPML
+if exist('gp', 'file')
+    warning('GPML may already exist. You should remove that version so that the correct version will be used.');
+end
+addpath(fullfile(mydir, 'dependencies', 'gpml-old'))
+addpath(fullfile(mydir, 'dependencies', 'gpml-old', 'cov'))
+addpath(fullfile(mydir, 'dependencies', 'gpml-old', 'doc'))
+addpath(fullfile(mydir, 'dependencies', 'gpml-old', 'inf'))
+addpath(fullfile(mydir, 'dependencies', 'gpml-old', 'lik'))
+addpath(fullfile(mydir, 'dependencies', 'gpml-old', 'mean'))
+addpath(fullfile(mydir, 'dependencies', 'gpml-old', 'util'))
+addpath(fullfile(mydir, 'dependencies', 'gpml-old', 'prior'))
+
+% NEXTGP
+addpath(genpath(fullfile(mydir, 'dependencies', 'nextgp')));
+
+% active_gp_hyperlearning
+addpath(fullfile(mydir, 'dependencies', 'active_gp_hyperlearning'));
+
+% gpml_extensions
+addpath(genpath(fullfile(mydir, 'dependencies', 'gpml_extensions')));
+
+% mgp
+addpath(fullfile(mydir, 'dependencies', 'mgp'));
+
+clear me mydir
