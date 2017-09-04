a9e6aab6d63e6816c419217a748a2f1970cf4f8a
Truong Nghiem <truong.nghiem@gmail.com>
2017-09-04 07:45:42 -0400

    Add submodule GPdyn.

--- code/setup_ICCPS18.m
+++ code/setup_ICCPS18.m
@@ -3,6 +3,8 @@
 me = mfilename;                                            % what is my filename
 mydir = which(me); mydir = mydir(1:end-3-numel(me));        % where am I located
 
+addpath(fullfile(mydir, 'dependencies'))
+
 % GPML
 switch is_mfile_mine('gp.m', fullfile(mydir, 'dependencies', 'gpml-old'))
     case 1
@@ -26,7 +28,7 @@ switch is_mfile_mine('minFunc.m', fullfile(mydir, 'dependencies', 'gpml-old', 'u
     case 1
         disp('minFunc is already added.');
     case 2
-        warning('A different minFunc already exists. You should remove that version so that the correct version will be used.');
+        warning('A different minFunc already exists. You may want to remove that version so that this version will be used.');
     otherwise
         addpath(fullfile(mydir, 'dependencies', 'gpml-old', 'util', 'minfunc'))
         addpath(fullfile(mydir, 'dependencies', 'gpml-old', 'util', 'minfunc', 'compiled'))
@@ -43,5 +45,18 @@ addpath(genpath(fullfile(mydir, 'dependencies', 'gpml_extensions')));
 
 % mgp
 addpath(fullfile(mydir, 'dependencies', 'mgp'));
+
+% GPDyn
+switch is_mfile_mine('trainGParx.m', fullfile(mydir, 'dependencies', 'GPdyn', 'gpdyn-training'))
+    case 1
+        disp('GPdyn is already added.');
+    case 2
+        warning('A different version of GPdyn already exists. You should remove that version so that the correct version will be used.');
+    otherwise
+        addpath(fullfile(mydir, 'dependencies', 'GPdyn', 'gpdyn-gp-evaluation'));
+        addpath(fullfile(mydir, 'dependencies', 'GPdyn', 'gpdyn-lmgp-evaluation'));
+        addpath(fullfile(mydir, 'dependencies', 'GPdyn', 'gpdyn-training'));
+        addpath(fullfile(mydir, 'dependencies', 'GPdyn', 'gpdyn-utilities'));
+end
 
 clear me mydir
