92e0783c43c3cec8a5b752ef281419ff66ea6c62
Truong Nghiem <truong.nghiem@gmail.com>
2017-09-25 18:26:41 -0400

    Results for OED and training with ramp limits.

--- code/training/feature_selection/plot_results.m
+++ code/training/feature_selection/plot_results.m
@@ -1,13 +1,14 @@
 function plot_results
-%plot_for_postfixes({'3days_final', '7days_final', '14days_final'}, 'SE');
-plot_for_postfixes({'3days_truongkernel', '7days_truongkernel', '14days_truongkernel'}, 'Truong''s kernel');
+%plot_for_postfixes({'3days_final', '7days_final', '14days_final'}, '', 'SE');
+plot_for_postfixes({'3days_truongkernel', '7days_truongkernel', '14days_truongkernel'}, '', 'Truong''s kernel');
+plot_for_postfixes({'3days_truongkernel', '4days_truongkernel', '7days_truongkernel', '14days_truongkernel'}, '2ramped_', 'RAMPED - Truong''s kernel');
 end
 
-function plot_for_postfixes(file_postfixes, txt)
+function plot_for_postfixes(file_postfixes, file_midfix, txt)
 
-file_prefixes = {'doe_sampling_noreset_IG_3input_ahead00',... % 'doe_sampling_noreset_IG_3input_ahead00_prior',...
-    'random_sampling_uniform_3input_ahead00',...
-    'random_sampling_prbs_3input_ahead00'};
+file_prefixes = {['doe_sampling_noreset_IG_' file_midfix '3input_ahead00'],... % 'doe_sampling_noreset_IG_3input_ahead00_prior',...
+    ['random_sampling_uniform_' file_midfix '3input_ahead00']}; %,...
+    %['random_sampling_prbs_' file_midfix '3input_ahead00']};
 
 result_rmse = nan(numel(file_postfixes), numel(file_prefixes));
 result_msll = result_rmse;
@@ -26,10 +27,10 @@ clear result
 figure
 bar(result_rmse);
 title([txt ' - RMSE']);
-legend('OED', 'uniform', 'prbs'); % 'OED-prior', 
+legend('OED', 'uniform'); % 'OED-prior', , 'prbs'
 
 figure
 bar(result_msll);
 title([txt ' - MSLL']);
-legend('OED', 'uniform', 'prbs'); %'OED-prior', 
+legend('OED', 'uniform'); %'OED-prior', , 'prbs'
 end
