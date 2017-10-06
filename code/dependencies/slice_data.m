16c9331171f7601982e905f9b4545ea6e5561bf6
Truong Nghiem <truong.nghiem@gmail.com>
2017-10-06 18:56:19 -0400

    slice_data now can take elements until the end.

--- code/dependencies/slice_data.m
+++ code/dependencies/slice_data.m
@@ -1,7 +1,12 @@
 function data = slice_data(data, len)
 % Slide fields of a data structure.
 fldnames = fieldnames(data);
+toend = isscalar(len);
 for k = 1:numel(fldnames)
-    data.(fldnames{k}) = data.(fldnames{k})(len(1):len(2),:);
+    if toend
+        data.(fldnames{k}) = data.(fldnames{k})(len:end,:);
+    else
+        data.(fldnames{k}) = data.(fldnames{k})(len(1):len(2),:);
+    end
 end
 end
